46fdab6497e99d27fcfe95e54269eaa1eaaae700
Ard Koornstra <448294@student.fontys.nl>
2024-07-22 16:44:50 +0200

    Input detection

--- SpeedySaucer/player.gd
+++ SpeedySaucer/player.gd
@@ -1,14 +1,12 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	test()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func test():
-	print("UwU")
+func _physics_process(delta):
+	var force = 1000
+	if Input.is_action_pressed("left"):
+		apply_force(Vector2(-force,0))
+	if Input.is_action_pressed("right"):
+		apply_force(Vector2(force,0))
+	if Input.is_action_pressed("up"):
+		apply_force(Vector2(0,-force))
+	if Input.is_action_pressed("down"):
+		apply_force(Vector2(0,force))
